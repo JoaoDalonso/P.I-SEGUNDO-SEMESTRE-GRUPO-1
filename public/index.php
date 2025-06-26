cf6ac0623a9d42689a749dfb5737d4e3a0424492
JoaoDalonso <jjdalonso@gmail.com>
2025-06-26 16:42:38 -0300

    Delete public directory

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php include 'includes/header.php'; ?>
-<section class="hero">
-  <div class="container hero-inner">
-    <h1>VENHA CONHECER!</h1>
-    <p>JC METAIS - Excelência em Montagens e Soldagens</p>
-    <a href="agendar.php" class="btn btn-primary">Agende Já</a>
-  </div>
-</section>
-<?php include 'includes/footer.php'; ?>
