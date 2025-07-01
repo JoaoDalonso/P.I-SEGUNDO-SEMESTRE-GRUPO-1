b88e67e538c162b3f0fee595e9d1f36a3fba2699
Vitor-Pimenta <vitor.almeida29@fatec.sp.gov.br>
2025-07-01 16:00:40 -0300

    Delete blog.php

--- blog.php
+++
@@ -1,2 +0,0 @@
-<?php include 'includes/header.php';?>
-<section class="content-page"><div class="container"><h2>Blog</h2><p>Em breve novidades!</p></div></section><?php include 'includes/footer.php';?>
